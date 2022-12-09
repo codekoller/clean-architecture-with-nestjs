4ef1ccd08e49ee0afa23b3d83e99df5b4609cb4b
William Koller <williamkoller404@gmail.com>
2022-12-09 17:53:16 -0300

    chore: add setGlobalPrefix

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix('/api');
   const config = app.get<ConfigService>(ConfigService);
   const port = config.get<number>('PORT');
   await app.listen(port, () => logger.log(`Server listening at ${port}`));
