8bb48ca9a0da969a99104bf2ae73ae89a73a6699
William Koller <williamkoller404@gmail.com>
2022-12-09 23:11:19 -0300

    refactor: adjust exception error

--- src/usecases/account/add-account.usecase.ts
+++ src/usecases/account/add-account.usecase.ts
@@ -1,19 +1,22 @@
 import { AccountRepositoryInterface } from '@app/data/protocols/account/add-account-repository.interface';
+import { ExceptionsService } from '@app/infra/exceptions/exceptions.service';
 import { AddAccountDto } from '@app/presentation/dtos/account/add-account.dto';
 import { AccountMapper } from '@app/presentation/mappers/account/account.mapper';
 import { ResponseAccountType } from '@app/presentation/types/account/response-account.type';
-import { ConflictException } from '@nestjs/common';
 
 export class AddAccountUseCase {
-  constructor(private readonly accountRepo: AccountRepositoryInterface) {}
+  constructor(
+    private readonly accountRepo: AccountRepositoryInterface,
+    private readonly exceptionService: ExceptionsService,
+  ) {}
 
   public async execute(data: AddAccountDto): Promise<ResponseAccountType> {
     const account = await this.accountRepo.findByEmail(data.email);
 
     if (account) {
-      throw new ConflictException(
-        'There is already an account with this email.',
-      );
+      this.exceptionService.conflictException({
+        message: 'There is already an account with this email.',
+      });
     }
 
     const accountCreated = await this.accountRepo.add(data);
